35404db7f2aaa34e26caa6fa68f3d5b69282e5ab
ventsi <v.saraminev@abv.bg>
2021-03-10 18:02:57 +0200

    Changed folder structure

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Modal.module.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 const Modal = (props) => {
